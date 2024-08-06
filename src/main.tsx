9eb6a7318f4099de60b9972626f38ac3bf394ca2
Artyom Afonin <0x000552@gmail.com>
2024-08-06 12:05:17 +0800

    init vite react ts swc project

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+//import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const rootElement = document.getElementById('root');
+const rootReact = ReactDOM.createRoot(rootElement!);
+
+rootReact.render(<p>test</p>);
